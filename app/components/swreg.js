2088058551c45b7866a8970bb4c4d12a06bfa7aa
andreilivshits <andreilivshits@gmail.com>
2023-09-08 04:21:27 +0000

    again

--- app/components/swreg.js
+++ app/components/swreg.js
@@ -3,7 +3,7 @@ import { useEffect } from "react"
 
 export default function RegisterSW() {
     useEffect(async ()=>{
-        await import("~/scripts/registerSw");
+        await import("../scripts/registerSW");
     });
     return (
         <></>
