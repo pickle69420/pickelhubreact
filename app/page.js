7f20bbb48c5fabf54d0677f5b9e094bfd6c44ace
andreilivshits <andreilivshits@gmail.com>
2023-09-08 04:19:17 +0000

    registered sw lol

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import SearchBar from './components/searchbar'
+import RegisterSW from './components/swreg'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <RegisterSW></RegisterSW>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <SearchBar></SearchBar>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
