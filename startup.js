f82bc52944c34bda6e2ea5a6066446ff4e576e04
andreilivshits <andreilivshits@gmail.com>
2023-09-09 04:13:30 +0000

    watevah

--- startup.js
+++ startup.js
@@ -1,33 +1,49 @@
 const express = require('express');
 const http = require('http');
 const next = require('next');
+const { parse } = require('url');
 const { createBareServer } = require('@tomphttp/bare-server-node');
-const { uvPath } = require('@titaniumnetwork-dev/ultraviolet');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const app = express();
+const nextOptions = {
+  target: 'http://localhost:3001',
+  changeOrigin: true,
+}
+const bareOptions = {
+  target: 'http://localhost:3002',
+  changeOrigin: true,
+}
+const nextProxy = createProxyMiddleware(nextOptions);
+const bareProxy = createProxyMiddleware(bareOptions);
 
-const httpServer = http.createServer()
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = /*'picklehubreact.hop.sh'*/ 'localhost';
+const port = 3001;
+const nextapp = next({ dev, hostname, port });
+const handle = nextapp.getRequestHandler();
 
-app.prepare()
-.then(() => {
-  const server = express()
-
-  server.get('*', (req, res) => {
-    return handle(req, res)
+nextapp.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true);
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err)
+      res.statusCode = 500
+      res.end('internal server error')
+    }
   })
+    .once('error', (err) => {
+      console.error(err)
+      process.exit(1)
+    })
+    .listen(port, () => {
+      console.log(`> PIKL Ready on http://${hostname}:${port}`)
+    })
+});
 
-  server.listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
-  })
-})
-.catch((ex) => {
-  console.error(ex.stack)
-  process.exit(1)
-})
-
+const httpServer = http.createServer();
 const bareServer = createBareServer("/bare/");
 
 httpServer.on("request", (req, res) => {
@@ -47,9 +63,21 @@ httpServer.on("upgrade", (req, socket, head) => {
 });
 
 httpServer.on("listening", () => {
-  console.log(`Bare listening on http://localhost:5000/`);
+  console.log(`Bare listening on http://localhost:3002/`);
 });
 
 httpServer.listen({
-  port: 5000
+  port: 3002
+});
+
+app.use('/', (req, res, next) => {
+  if (req.url.startsWith('/bare/')) {
+    bareProxy(req, res, next);
+  } else {
+    nextProxy(req, res, next);
+  }
+});
+
+app.listen(3000,()=>{
+  console.log('listening on 3000');
 });
